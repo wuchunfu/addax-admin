84bffa9a9ba4dc57dd22b92a52cbfec3e1211b49
wgzhao <wgzhao@gmail.com>
2021-11-16 17:05:46 +0800

    Add Exception

---
+++ src/main/java/com/wgzhao/addax/admin/exception/BaseCommonException.java
@@ -0,0 +1,29 @@
+package com.wgzhao.addax.admin.exception;
+
+
+public class BaseCommonException
+{
+    private String errorCode;
+    private String errorMessage;
+
+    public String getErrorCode()
+    {
+        return errorCode;
+    }
+
+    public void setErrorCode(String errorCode)
+    {
+        this.errorCode = errorCode;
+    }
+
+    public String getErrorMessage()
+    {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage)
+    {
+        this.errorMessage = errorMessage;
+    }
+
+}
