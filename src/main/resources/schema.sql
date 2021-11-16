df7f8215188055f936961cd5bb7122938877fdcd
wgzhao <wgzhao@gmail.com>
2021-11-16 22:38:06 +0800

    Add missing Copyright

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -213,6 +213,21 @@ CREATE TABLE `t_type_info`
   DEFAULT CHARSET = utf8 COMMENT ='数据库类型信息';
 
 
+/*
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+
 -- addax.t_user definition
 
 CREATE TABLE `t_user`
